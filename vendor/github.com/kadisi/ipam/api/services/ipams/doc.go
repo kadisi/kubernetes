29d9e64d2b9d6608def5f31cfb4ae20955071ae1
zhangjie <iamkadisi@163.com>
2019-03-07 11:04:19 +0800

    add ipams
    Signed-off-by: zhangjie <iamkadisi@163.com>

---
+++ vendor/github.com/kadisi/ipam/api/services/ipams/doc.go
@@ -0,0 +1,14 @@
+/*
+#  #############################################
+#  Copyright (c) 2019-2039 All rights reserved.
+#  #############################################
+#
+#  Name:  pods.pb.go
+#  Date:  2019-02-17 19:54
+#  Author:   zhangjie
+#  Email:   iamzhangjie0619@163.com
+#  Desc:
+#
+*/
+
+package ipams
